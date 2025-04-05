c13a575b0ccf31f02e104c154c7052db9ff120d9
sasith duleepa <duleepajayathissa@gmail.com>
2025-04-06 01:02:22 +0530

    page animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
 
-import Navbar from "./components/navbar/navbar";
+import Home from "./pages/home/home";
+import About from "./pages/about/about";
+import Contact from "./pages/contact/contact";
+import Projects from "./pages/projects/projects";
 
 export default function App() {
+  const [page, setPage] = useState(<Home />);
+  const [prevPage, setPrevPage] = useState(<Home />);
+
+  const [animClass, setAnimClass] = useState("right-left"); // default
   return (
-    <div>
-      <Navbar />
+    <div className="app">
+      <div className="sidebar">
+        <div className="sidebar-links-div">
+          <button
+            className="sidebar-btn"
+            onClick={() => {
+              setAnimClass(
+                animClass === "right-left" ? "right_left" : "right-left"
+              );
+              setPage(<Home />);
+              setTimeout(() => setPrevPage(<Home />), 1000);
+            }}
+          >
+            Home
+          </button>
+          <button
+            className="sidebar-btn"
+            onClick={() => {
+              setAnimClass(
+                animClass === "right-left" ? "right_left" : "right-left"
+              );
+              setPage(<About />);
+              setTimeout(() => setPrevPage(<About />), 1000);
+            }}
+          >
+            About
+          </button>
+
+          <button
+            className="sidebar-btn"
+            onClick={() => {
+              setAnimClass(
+                animClass === "right-left" ? "right_left" : "right-left"
+              );
+              setPage(<Projects />);
+              setTimeout(() => setPrevPage(<Projects />), 1000);
+            }}
+          >
+            Projects
+          </button>
+          <button
+            className="sidebar-btn"
+            onClick={() => {
+              setAnimClass(
+                animClass === "right-left" ? "right_left" : "right-left"
+              );
+              setPage(<Contact />);
+              setTimeout(() => setPrevPage(<Contact />), 1000);
+            }}
+          >
+            Contact
+          </button>
+        </div>
+      </div>
+      <div className="page-div ">
+        <div className="prev-page">{prevPage}</div>
+        <div className={`page ${animClass}`}>{page}</div>
+      </div>
     </div>
   );
 }
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./home.css";
+
+export default function Home() {
+  return (
+    <div className="home">
+      {/* <div className="home-background">
+        <div className="background-up">
+          <p className="background-up-p">HELLO!</p>
+        </div>
+        <div className="background-down">
+          <p className="background-down-p">WELCOME TO MY WORLD!</p>
+        </div>
+      </div>
+
+      <div className="home-main">
+        <p></p>
+        <div className="main-intro">
+          <p className="main-intro-p">Hello, I am a</p>
+          <div className="table center">
+            <div className="monitor-wrapper center">
+              <p class="monitor center">
+                | front-end developer | back-end developer | full-stack
+                developer | IoT System Developer |
+              </p>
+            </div>
+          </div>
+        </div>
+        <p contentEditable className="home_name">
+          Sasith Duleepa
+        </p>
+      </div> */}
+    </div>
+  );
+}
