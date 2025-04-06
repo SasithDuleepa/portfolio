6a5e75c6fbe81ba7f4c5d977637bf6e36b96b603
sasith duleepa <duleepajayathissa@gmail.com>
2025-04-07 01:14:57 +0530

    about me

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -2,5 +2,88 @@ import React from "react";
 import "./about.css";
 
 export default function About() {
-  return <div className="about"></div>;
+  return (
+    <div className="about">
+      <div className="about-title-div">
+        <p className="text-header-main">About Me</p>
+        <div className="about-title-line1"></div>
+        <div className="about-title-line2"></div>
+      </div>
+
+      <div className="about-intro-div">
+        <p className="text-header">I'm sasith and a</p>
+        <p className="about-intro-p">Full-stack developer</p>
+      </div>
+
+      <p className="text-content about-content">
+        is simply dummy text of the printing and typesetting industry. Lorem
+        Ipsum has been the industry's standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a
+        type specimen book. It has survived not only five centuries, but also
+        the leap into electronic typesetting, remaining essentially unchanged.
+        It was popularised in the 1960s with the release of Letraset sheets
+        containing Lorem Ipsum passages, and more recently
+      </p>
+
+      <div className="about-skills-container">
+        <div className="about-skills-left-div">
+          <div className="about-skills-left-p-div">
+            <p className="about-skills-left-p1">Birthday :</p>
+            <p className="about-skills-left-p2">1997 December 08</p>
+          </div>
+
+          <div className="about-skills-left-p-div">
+            <p className="about-skills-left-p1">Birthday :</p>
+            <p className="about-skills-left-p2">1997 December 08</p>
+          </div>
+
+          <div className="about-skills-left-p-div">
+            <p className="about-skills-left-p1">Birthday :</p>
+            <p className="about-skills-left-p2">1997 December 08</p>
+          </div>
+
+          <div className="about-skills-left-p-div">
+            <p className="about-skills-left-p1">Birthday :</p>
+            <p className="about-skills-left-p2">1997 December 08</p>
+          </div>
+
+          <div className="about-skills-left-p-div">
+            <p className="about-skills-left-p1">Birthday :</p>
+            <p className="about-skills-left-p2">1997 December 08</p>
+          </div>
+
+          <div className="about-skills-left-p-div">
+            <p className="about-skills-left-p1">Birthday :</p>
+            <p className="about-skills-left-p2">1997 December 08</p>
+          </div>
+
+          <div className="about-skills-left-p-div">
+            <p className="about-skills-left-p1">Birthday :</p>
+            <p className="about-skills-left-p2">1997 December 08</p>
+          </div>
+
+          <div className="about-skills-left-p-div">
+            <p className="about-skills-left-p1">Birthday :</p>
+            <p className="about-skills-left-p2">1997 December 08</p>
+          </div>
+
+          <div className="about-skills-left-p-div">
+            <p className="about-skills-left-p1">Birthday :</p>
+            <p className="about-skills-left-p2">1997 December 08</p>
+          </div>
+
+          <div className="about-skills-left-p-div">
+            <p className="about-skills-left-p1">Birthday :</p>
+            <p className="about-skills-left-p2">1997 December 08</p>
+          </div>
+
+          <div className="about-skills-left-btn-div">
+            <button className="about-skills-left-btn">Download CV</button>
+            <button className="about-skills-left-btn">Hire Me</button>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
