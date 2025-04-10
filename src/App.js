3c5714f3bcf587eee6e46c7e97e6b28a2c69feb0
sasith duleepa <duleepajayathissa@gmail.com>
2025-04-10 23:18:08 +0530

    projects

--- src/App.js
+++ src/App.js
@@ -44,9 +44,33 @@ export default function App() {
     setThemeColor(color);
   };
 
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="app" data-theme={theme} data-theme-color={themeColor}>
-      <div className="sidebar">
+      <button
+        onClick={() => {
+          console.log("clicked");
+          setMenuOpen(!menuOpen);
+        }}
+        className="menu-icon-btn"
+      >
+        {menuOpen === true ? (
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+            <path d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z"></path>
+          </svg>
+        ) : (
+          <svg
+            className="menu-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 50 50"
+          >
+            <path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"></path>
+          </svg>
+        )}
+      </button>
+
+      <div className={menuOpen ? "sidebar-show" : "sidebar"}>
         <div className="sidebar-links-div">
           <button
             className="sidebar-btn"
